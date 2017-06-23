b7baa2976ed9b11a76158d709e4fa5b175696746
Evgeny Poberezkin <e.poberezkin@me.com>
2017-06-23 20:17:05 +0100

    test: rule commit-name

---
+++ spec/rules/commit-name.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const execute = require('../../lib/execute');
+const assert = require('assert');
+const nock = require('nock');
+const githubMock = require('../execute/github_mock');
+
+
+describe('rule commit-name', function() {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('should check that commit names satisfy semantic commits rules', function() {
+    githubMock.mock('/repos/milojs/milo/commits?since=2017-04-23&per_page=30&page=1', '../fixtures/milojs_milo_commits');
+
+    const config = {
+      org: 'MailOnline',
+      repositories: {
+        'milojs/milo': {
+          rules: {
+            'commit-name': [2, {
+              types: ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+              maxSubjectLength: 50,
+              multiLine: true,
+              maxLineLength: 72
+            }]
+          }
+        }
+      }
+    };
+
+    return execute.checkRules(config, {commits: {since: '2017-04-23'}})
+    .then((results) => {
+      assert.deepStrictEqual(results, {
+        'milojs/milo': {
+          'commit-name': [
+            {
+              mode: 2,
+              message: "Commit names do not satisfy requirements:\nchore: component.isReaady = false on destroy (#80)\n\n* chore: component.isReaady = false on destroy\r\n\r\n* Update c_class.js\r\n\r\nkeeping `isReady` as such:\r\n\r\n  * `false` when not ready\r\n  * `true` only once dispatching `'stateready'`\r\n  * `undefined` when destroyed (the property is not shadowed 'cause I'm not sure if `false` is set as default in the prototype)\r\n\r\n* Update c_class.js\r\n\r\nBe sure `init()` sets `isReady` as `false`.",
+              valid: false
+            }
+          ]
+        }
+      });
+      assert(nock.isDone());
+    });
+  });
+});

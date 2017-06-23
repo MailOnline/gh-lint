206f9c792c98a9bfa33e1c9096ca69e29695df35
Evgeny Poberezkin <e.poberezkin@me.com>
2017-06-23 20:53:50 +0100

    test: rule repo-team

---
+++ spec/rules/repo-team.spec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const execute = require('../../lib/execute');
+const assert = require('assert');
+const nock = require('nock');
+const githubMock = require('../execute/github_mock');
+
+
+describe('rule repo-team', function() {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('should check that repo is assigned to one of specified teams', function() {
+    githubMock.repos.organization.MailOnline.list();
+    githubMock.repos.organization.MailOnline.teams();
+
+    const config = {
+      org: 'MailOnline',
+      rules: {
+        'repo-team': [2, {
+          teams: ['#ads', '#cc', '#clj', '#ios-ny', '#ml-nlp', '#mol-fe', '#rc', '#rta', '#support', '#systems', 'Metro', 'IOS']
+        }]
+      }
+    };
+
+    return execute.checkRules(config)
+    .then((results) => {
+      assert.deepStrictEqual(results, require('../fixtures/repo-team_expected_results.json'));
+      assert(nock.isDone());
+    });
+  });
+});

d4d1d5d86e1c2685e5b06ee5ccda0e197581cc8a
Evgeny Poberezkin <e.poberezkin@me.com>
2017-10-14 21:20:51 +0100

    feat: rule repo-single-admin, option singleTeam in repo-team removed

--- spec/rules/repo-team.spec.js
+++ spec/rules/repo-team.spec.js
@@ -51,26 +51,4 @@ describe('rule repo-team', () => {
       assert(nock.isDone());
     });
   });
-
-  it('should check that repo is assigned to one of specified teams and ONLY ONE team has specified permission (admin)', () => {
-    githubMock.repos.organization.MailOnline.list();
-    githubMock.repos.organization.MailOnline.teams();
-
-    const config = {
-      org: 'MailOnline',
-      rules: {
-        'repo-team': [2, {
-          teams: ['#ads', '#cc', '#clj', '#ios-ny', '#ml-nlp', '#mol-fe', '#rc', '#rta', '#support', '#systems', 'Metro', 'IOS'],
-          singleTeam: true
-          // minPermission: 'admin' // this is default
-        }]
-      }
-    };
-
-    return execute.checkRules(config)
-    .then((results) => {
-      assert.deepStrictEqual(results, require('../fixtures/repo-team_single-team_expected_results.json'));
-      assert(nock.isDone());
-    });
-  });
 });
